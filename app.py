98beccdc752e8d579294ec4a80bad2c1bb1a5cc1
jeffersonamado <jeffersonamado@gmail.com>
2023-08-19 09:39:46 -0500

    feat: add CRUD to products

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def pong():
         "message":"pong"
     }), 200
 
-@app.route("/product", methods=['POST'])
-def newproduct():
+@app.route("/products", methods=['POST'])
+def new_product():
     data = request.json
     try:
       a_product = Product(
@@ -39,5 +39,38 @@ def newproduct():
       return jsonify({"message": "success", 'id':a_product.id}), 200
     except Exception as ex:
        return jsonify({"message": str(ex)}), 400
+    
+@app.route("/products", methods=['GET'])
+def products():
+   return jsonify([
+      a_product.serialize() for a_product in Product.query.all()
+   ])
+
+@app.route("/products/<int:id>", methods=['GET'])
+def get_product(id):
+   a_product = Product.query.get_or_404(id)
+   return jsonify(a_product.serialize())
+
+@app.route("/products/<int:id>", methods=['PUT'])
+def edit_product(id):
+   a_product = Product.query.get_or_404(id)
+   data = request.json
+   try:
+      a_product.product_name = data['name']
+      a_product.product_description = data['description']
+      a_product.product_price = data['price']
+      a_product.availability = data['availability']
+      db.session.commit()
+      return jsonify(a_product.serialize())
+   except Exception as ex:
+      return jsonify({"message": str(ex)}), 400
+
+@app.route("/products/<int:id>", methods=['DELETE'])
+def delete_product(id):
+   a_product = Product.query.get_or_404(id)
+   a_product.availability = 0
+   db.session.commit()
+   return jsonify(a_product.serialize())
+
 
 app.run(host="0.0.0.0", debug=True, port=9030)
